8c1291e9fc5e497071a04ca9c857c567f2fa5912
Victor <victormazzola2019@gmail.com>
2026-09-14 20:44:35 -0300

    initial commit Spring Boot backend

---
+++ src/main/java/br/com/tech4change/accessible_care_api/AccessibleCareApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.tech4change.accessible_care_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessibleCareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessibleCareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tech4change/accessible_care_api/AccessibleCareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tech4change.accessible_care_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessibleCareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
