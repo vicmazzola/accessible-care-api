faacb03472d6bad32c013f67eb82878f770977fb
Victor <victormazzola2019@gmail.com>
2026-09-18 21:10:59 -0300

    refactor: remove obsolete FastAPI model integration

--- src/main/java/br/com/tech4change/accessible_care_api/dto/PredictionResponse.java
+++
@@ -1,9 +0,0 @@
-package br.com.tech4change.accessible_care_api.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record PredictionResponse(
-        @JsonProperty("class")
-        String className,
-        double confidence
-) {}
